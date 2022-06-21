a1f676c9dd124c301676fa79d96e1be9e38e6bfa
Di Honor <dmitry.metelitsa@gmail.com>
2022-06-21 07:23:25 +0300

    added es Spain translate

--- lib/main.dart
+++ lib/main.dart
@@ -44,6 +44,7 @@ class MyApp extends StatelessWidget {
         Locale('pl', ''),
         Locale('be', ''),
         Locale('uk', ''),
+        Locale('es', ''),
       ],
       scrollBehavior: const ConstantScrollBehavior(),
       theme: ThemeData(
